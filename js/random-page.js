16ac563e947912e90ffbbe8eb9bb719567a7746f
0And1Story <witherman233@gmail.com>
2019-08-09 18:12:34 +0800

    Add tales

--- js/random-page.js
+++ js/random-page.js
@@ -1,3 +1,11 @@
 function randomSCP() {
-    window.location.href = 'scp-yc-' + addZero(Math.ceil(Math.random() * page_num)).toString() + '.html';
+    window.location.href = '../scp-yc/scp-yc-' + addZero(Math.ceil(Math.random() * page_num)).toString() + '.html';
+}
+
+var tales = [
+    'foundation-criminal-case-logs'
+];
+
+function randomTale() {
+    window.location.href = '../tales/' + tales[Math.floor(Math.random() * tales.length)] + '.html';
 }
